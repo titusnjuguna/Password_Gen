7ce8e68214aeb63e3be8e8a9ba656257aafc5939
vector star <titusnjuguna59@gmail.com>
2021-07-22 01:25:07 +0300

    Delete comment lines
    Delete trial code

--- passwordGen.py
+++ passwordGen.py
@@ -1,9 +1,5 @@
 import string
 import random
-#characters = string.ascii_lowercase + string.ascii_uppercase + string.punctuation  + string.digits
-#password =  "".join(choice(characters) for x in range(0,8))
-#print(password)
-
 def password_gen():
     lo_wer = random.sample(string.ascii_lowercase,2)
     up_per = random.sample(string.ascii_uppercase,2)
